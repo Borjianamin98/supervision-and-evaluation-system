35f8ce6a18707956af9f2c48b87280ec1b292dee
Amin <borjianamin98@outlook.com>
2021-03-25 10:01:29 +0430

    Add user model in app server

---
+++ app-server/src/main/java/ir/ac/sbu/evaluation/model/User.java
@@ -0,0 +1,38 @@
+package ir.ac.sbu.evaluation.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+public class User extends BaseEntity {
+
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        super(id);
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
