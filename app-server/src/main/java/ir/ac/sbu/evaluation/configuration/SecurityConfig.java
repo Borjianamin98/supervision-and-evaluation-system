9f08e4a73bcb91715afd4ac8b5db7cf3fa762674
Amin <borjianamin98@outlook.com>
2021-03-25 16:52:34 +0430

    Add in memory authentication and configure http security for cors, csrf and authentication paths

--- app-server/src/main/java/ir/ac/sbu/evaluation/configuration/SecurityConfig.java
+++ app-server/src/main/java/ir/ac/sbu/evaluation/configuration/SecurityConfig.java
@@ -1,19 +1,43 @@
 package ir.ac.sbu.evaluation.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(getPasswordEncoder().encode("admin")).roles("USER")
+                .and()
+                .passwordEncoder(getPasswordEncoder());
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers("/h2-console/**").permitAll();
-
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+                .antMatchers( "/favicon.ico").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/**").hasRole("USER")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        // Disable creation of session cookie in browser
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().disable(); // Allow all Cross-Origin Resource for sharing
+        http.csrf().disable(); // Disable Cross-site request forgery
+        http.headers().frameOptions().disable(); // Disable X-Frame-Options in response headers
     }
 }
