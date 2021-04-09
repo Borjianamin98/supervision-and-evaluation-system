0992d6e52d8bb184a819569f4d0aa6b6396dfacd
Amin <borjianamin98@outlook.com>
2021-04-09 13:10:30 +0430

    Move app bar functionality to nav bar component

--- app-frontend/src/components/NavBar/NavBar.tsx
+++ app-frontend/src/components/NavBar/NavBar.tsx
@@ -1,8 +1,11 @@
+import AppBar from "@material-ui/core/AppBar";
 import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
+import Toolbar from "@material-ui/core/Toolbar";
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import MenuIcon from "@material-ui/icons/Menu";
 import classNames from 'classnames';
 import React from 'react';
 
@@ -10,6 +13,21 @@ const NAV_BAR_WIDTH = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+            transition: theme.transitions.create(['width', 'margin'], {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.leavingScreen,
+            }),
+        },
+        appBarShift: {
+            width: `calc(100% - ${NAV_BAR_WIDTH}px)`,
+            transition: theme.transitions.create(['margin', 'width'], {
+                easing: theme.transitions.easing.easeOut,
+                duration: theme.transitions.duration.enteringScreen,
+            }),
+            marginRight: NAV_BAR_WIDTH,
+        },
         drawer: {
             width: NAV_BAR_WIDTH,
             flexShrink: 0,
@@ -29,43 +47,74 @@ const useStyles = makeStyles((theme: Theme) =>
             overflowX: 'hidden',
             width: theme.spacing(7) + 1,
         },
+        menuButton: {
+            marginLeft: 18,
+        },
+        menuButtonHidden: {
+            display: 'none',
+        },
         offset: theme.mixins.toolbar
     }),
 );
 
 interface NavBarProps {
-    open: boolean,
-    onClose: () => void,
+    appBarContent: React.ReactElement
 }
 
 const NavBar: React.FunctionComponent<NavBarProps> = (props) => {
     const classes = useStyles();
 
+    const [open, setOpen] = React.useState<boolean>(false);
+    const navBarOnOpenHandler = () => {
+        setOpen(true);
+    };
+    const navBarOnCloseHandler = () => {
+        setOpen(false);
+    };
+
     return (
-        <Drawer
-            variant="permanent"
-            anchor="right"
-            className={classNames(classes.drawer, {
-                [classes.drawerOpen]: props.open,
-                [classes.drawerClose]: !props.open,
-            })}
-            classes={{
-                paper: classNames({
-                    [classes.drawerOpen]: props.open,
-                    [classes.drawerClose]: !props.open,
-                }),
-            }}
-        >
-            <div className={classes.offset}>
-                <IconButton onClick={props.onClose}>
-                    <ChevronRightIcon/>
-                </IconButton>
-            </div>
-            <Divider/>
-            {props.children}
-        </Drawer>
+        <div>
+            <AppBar
+                position="fixed"
+                className={classNames(classes.appBar, {[classes.appBarShift]: open})}
+            >
+                <Toolbar dir="rtl">
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={navBarOnOpenHandler}
+                        className={classNames(classes.menuButton, {[classes.menuButtonHidden]: open})}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    {props.appBarContent}
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                anchor="right"
+                className={classNames(classes.drawer, {
+                    [classes.drawerOpen]: open,
+                    [classes.drawerClose]: !open,
+                })}
+                classes={{
+                    paper: classNames({
+                        [classes.drawerOpen]: open,
+                        [classes.drawerClose]: !open,
+                    }),
+                }}
+            >
+                <div className={classes.offset}>
+                    <IconButton onClick={navBarOnCloseHandler}>
+                        <ChevronRightIcon/>
+                    </IconButton>
+                </div>
+                <Divider/>
+                {props.children}
+            </Drawer>
+        </div>
     );
 }
 
-export {NAV_BAR_WIDTH};
 export default NavBar;
--- app-frontend/src/views/DashboardView.tsx
+++ app-frontend/src/views/DashboardView.tsx
@@ -1,11 +1,6 @@
-import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
 import {createStyles, makeStyles, Theme} from '@material-ui/core/styles';
-import Toolbar from '@material-ui/core/Toolbar';
-import MenuIcon from '@material-ui/icons/Menu';
-import classNames from 'classnames';
 import React from 'react';
-import NavBar, {NAV_BAR_WIDTH} from "../components/NavBar/NavBar";
+import NavBar from "../components/NavBar/NavBar";
 import DashboardAppBar from "./dashboard/DashboardAppBar";
 import DashboardContentRoutes from "./dashboard/DashboardContentRoutes";
 import DashboardNavBarLinks from "./dashboard/DashboardNavBarLinks";
@@ -15,70 +10,24 @@ const useStyles = makeStyles((theme: Theme) =>
         root: {
             display: 'flex',
         },
-        appBar: {
-            zIndex: theme.zIndex.drawer + 1,
-            transition: theme.transitions.create(['width', 'margin'], {
-                easing: theme.transitions.easing.sharp,
-                duration: theme.transitions.duration.leavingScreen,
-            }),
-        },
-        appBarShift: {
-            width: `calc(100% - ${NAV_BAR_WIDTH}px)`,
-            transition: theme.transitions.create(['margin', 'width'], {
-                easing: theme.transitions.easing.easeOut,
-                duration: theme.transitions.duration.enteringScreen,
-            }),
-            marginRight: NAV_BAR_WIDTH,
-        },
-        menuButton: {
-            marginLeft: 18,
-        },
-        menuButtonHidden: {
-            display: 'none',
-        },
-        offset: theme.mixins.toolbar,
         content: {
             flexGrow: 1,
             padding: theme.spacing(3)
-        }
+        },
+        offset: theme.mixins.toolbar
     }),
 );
 
 const DashboardView: React.FunctionComponent = () => {
     const classes = useStyles();
 
-    const [open, setOpen] = React.useState<boolean>(false);
-    const navBarOnOpenHandler = () => {
-        setOpen(true);
-    };
-    const navBarOnCloseHandler = () => {
-        setOpen(false);
-    };
-
     return (
         <div className={classes.root}>
-            <AppBar
-                position="fixed"
-                className={classNames(classes.appBar, {[classes.appBarShift]: open})}
-            >
-                <Toolbar dir="rtl">
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="end"
-                        onClick={navBarOnOpenHandler}
-                        className={classNames(classes.menuButton, {[classes.menuButtonHidden]: open})}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <DashboardAppBar/>
-                </Toolbar>
-            </AppBar>
             <main dir="rtl" className={classes.content}>
                 <div className={classes.offset}/>
                 <DashboardContentRoutes/>
             </main>
-            <NavBar open={open} onClose={navBarOnCloseHandler}>
+            <NavBar appBarContent={<DashboardAppBar/>}>
                 <DashboardNavBarLinks/>
             </NavBar>
         </div>
