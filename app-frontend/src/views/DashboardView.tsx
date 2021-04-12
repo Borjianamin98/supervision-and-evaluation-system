7832a4c612137a7f63c236f8c54603a6d81316b5
Amin <borjianamin98@outlook.com>
2021-04-13 00:42:19 +0430

    Add first card item in dashboard to add/view list of projects

--- app-frontend/src/views/DashboardView.tsx
+++ app-frontend/src/views/DashboardView.tsx
@@ -1,42 +1,52 @@
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import {Card, CardMedia, makeStyles} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
-import React, {useEffect, useState} from 'react';
-import {useLocation} from "react-router-dom";
-import apiAxios from "../config/axios-config";
-import {User} from "../model/user";
-import {API_USER_PATH} from "../services/ApiPaths";
+import React from 'react';
+import addDocumentImage from "../assets/images/dashboard/add-document.jpg";
 
-interface UserApiResponse {
-    users: Array<User>,
-}
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+});
 
 const DashboardView: React.FunctionComponent = () => {
-    const location = useLocation();
-
-    const [users, setUsers] = useState<Array<User>>([]);
-
-    useEffect(() => {
-        apiAxios.get<UserApiResponse>(API_USER_PATH)
-            .then(value => setUsers(value.data.users))
-            .catch(err => console.log("error"));
-    }, [location.key])
+    const classes = useStyles();
 
     return (
-        <div dir="rtl">
-            <Typography paragraph>
-                نام کاربران که به صورت امتحانی صرفا دریافت شده و نمایش داده شده است:
-            </Typography>
-            <List>
-                {
-                    users.map(u => <ListItem dir="rtl" key={u.username}>
-                        <ListItemText primary={u.username}/>
-                    </ListItem>)
-                }
-            </List>
+        <div dir="rtl" style={{display: "flex", flexGrow: 0}}>
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        alt="Add project"
+                        height="140"
+                        image={addDocumentImage}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5">
+                            افزودن پروژه
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary">
+                            پروژه‌ی دوره تحصیلی خود را برای ارائه نمودن اضافه نمایید.
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        اضافه کردن
+                    </Button>
+                    <Button size="small" color="primary">
+                        مشاهده لیست
+                    </Button>
+                </CardActions>
+            </Card>
         </div>
-    );
+    )
+        ;
 }
 
 export default DashboardView;
