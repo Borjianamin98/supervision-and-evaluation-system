a87f66b257c88684bc7ac10643f9ebcc4d7600ce
Amin <borjianamin98@outlook.com>
2021-04-14 00:03:32 +0430

    Replace authentication error HTML with JSON response

--- app-server/src/main/java/ir/ac/sbu/evaluation/security/JwtAuthenticationEntryPoint.java
+++ app-server/src/main/java/ir/ac/sbu/evaluation/security/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,10 @@
 package ir.ac.sbu.evaluation.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -10,11 +14,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper;
+
+    public JwtAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException {
         // This is invoked when user tries to access a secured resource without supplying any credentials.
         // We should just send a 401 unauthorized response.
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        Map<String, Object> data = new HashMap<>();
+        data.put("timestamp", Calendar.getInstance().getTime());
+        data.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+        data.put("error", "Unauthorized");
+        data.put("message", authException.getMessage());
+        data.put("path", request.getRequestURI());
+        response.getWriter().write(objectMapper.writeValueAsString(data));
     }
 }
